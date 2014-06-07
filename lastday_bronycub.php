8148caff56e97f47fdcd5179bdb89ae051c6bd14
Adrien Sohier <adrien.sohier@art-software.fr>
2014-06-07 23:18:53 +0200

    Modification script de récupération du log pour n'afficher que l'heure

--- lastday_bronycub.php
+++ lastday_bronycub.php
@@ -3,7 +3,7 @@ header('Content-Type: text/plain; charset=utf8');
 $handle = popen('grep "$(date +%Y-%m-%d)" /home/berry-punch/logs/ChannelLogger/freenode/#bronycub/#bronycub.log 2>&1', 'r');
 
 while (!feof($handle)) {
-	$buffer = fgets($handle);
+	$buffer = substr(fgets($handle),11);
 	# $buffer = preg_replace(array('/</', '/>/', "/\n/"), array('&lt;','&gt;', "<br />\n"), $buffer);
 	$buffer = str_replace(array('has joined #bronycub', 'has left #bronycub', 'has quit IRC'), array('est entré sur le canal', 'est parti du canal', 's\'est déconnecté'), $buffer);
 	echo "$buffer";
