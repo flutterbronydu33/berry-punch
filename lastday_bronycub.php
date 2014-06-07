9f95ba31c2a5d3be867cbaf132ccb5bff0539d6c
Adrien Sohier <adrien.sohier@art-software.fr>
2014-06-07 23:11:46 +0200

    Ajout d'un script pour récupérer la dernière journée du log

---
+++ lastday_bronycub.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: text/plain; charset=utf8');
+$handle = popen('grep "$(date +%Y-%m-%d)" /home/berry-punch/logs/ChannelLogger/freenode/#bronycub/#bronycub.log 2>&1', 'r');
+
+while (!feof($handle)) {
+	$buffer = fgets($handle);
+	# $buffer = preg_replace(array('/</', '/>/', "/\n/"), array('&lt;','&gt;', "<br />\n"), $buffer);
+	$buffer = str_replace(array('has joined #bronycub', 'has left #bronycub', 'has quit IRC'), array('est entré sur le canal', 'est parti du canal', 's\'est déconnecté'), $buffer);
+	echo "$buffer";
+	ob_flush();
+	flush();
+}
+pclose($handle);
