b1c01c372c175f487c7c4665ca549f24a36a357a
Adrien Sohier <adrien.sohier@art-software.fr>
2014-06-11 23:54:20 +0200

    Modification des buffers: ajout d'un flush
    flush des buffers toutes les 32768 lignes

--- SHBot.sh
+++ SHBot.sh
@@ -28,14 +28,15 @@ read_loop_inbuffer()
 {
 	local nb idx;
 	while [ -f pidfile ]; do
-		nb=$(cat in_buffer|wc -l);
+		nb=$(cat in_lnk|wc -l);
 		idx=$(cat $IN_IDX);
 		while [ $idx -lt $nb ] ; do
 			let idx++;
 			echo -n $idx > $IN_IDX;
-			sed -urne "${idx}p" in_buffer;
+			sed -urne "${idx}p" in_lnk;
 		done
 		sleep 0.1;
+		[ $idx -ge 32768 ] && flush_buffer_in
 	done
 }
 read_line_outbuffer()
@@ -43,23 +44,46 @@ read_line_outbuffer()
 	local nb idx;
 	idx=$(cat $OUT_IDX);
 
-	nb=$(cat out_buffer|wc -l);
+	nb=$(cat out_lnk|wc -l);
 	if [ $idx -lt $nb ]; then
 		let idx++;
 		echo -n $idx > $OUT_IDX;
-		sed -urne "${idx}p" out_buffer;
+		sed -urne "${idx}p" out_lnk;
 	fi
+	[ $idx -ge 32768 ] && flush_buffer_out
 }
 read_line_outbuffer_wait()
 {
 	local idx;
 
 	idx=$(cat $OUT_IDX);
-	while [ -f pidfile ] && [ $(cat out_buffer|wc -l) -le $idx ] ; do
+	while [ -f pidfile ] && [ $(cat out_lnk|wc -l) -le $idx ] ; do
 		sleep 0.1;
 	done
 
 	read_line_outbuffer;
+}
+flush_buffer_in()
+{
+	local cur new;
+	cur="$(readlink -f in_lnk)";
+	new="in_buffer.$(date +%s)";
+
+	touch "$new";
+	ln -svf "$new" "in_lnk"
+	echo "0" > $IN_IDX;
+	rm "$cur";
+}
+flush_buffer_out()
+{
+	local cur new;
+	cur="$(readlink -f out_lnk)";
+	new="out_buffer.$(date +%s)";
+
+	touch "$new";
+	ln -svf "$new" "out_lnk"
+	echo "0" > $OUT_IDX;
+	rm "$cur";
 }
 netlink()
 {
@@ -67,12 +91,14 @@ netlink()
 }
 
 # ---------- Main ----------
-rm in_buffer out_buffer OUT_IDX IN_IDX;
-touch in_buffer out_buffer;
+rm in_buffer out_buffer OUT_IDX IN_IDX; in_lnk out_lnk
+ln -sv in_buffer in_lnk;
+ln -sv out_buffer out_lnk;
+
+touch in_lnk out_lnk;
 echo -n "0" > $IN_IDX;
 echo -n "0" > $OUT_IDX;
 echo $$ > pidfile;
-
 
 netlink &
 echo "==> Started";
@@ -101,4 +127,4 @@ while [ -f pidfile ]; do
 		eval "${HOOKS["cmd_received"]}"
 	fi
 done
-rm IN_IDX OUT_IDX in_buffer out_buffer;
+rm IN_IDX OUT_IDX in_buffer.* out_buffer.* in_lnk out_lnk;
