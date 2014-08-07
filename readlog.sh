fe899de5956b563da0078770ef8e5e99deec5009
Adrien Sohier <adrien.sohier@art-software.fr>
2014-08-07 11:29:33 +0200

    Modif readlog (log sur un mois)

--- readlog.sh
+++ readlog.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-cat old_logs/#bronycub.$(date +%Y%m)* \#bronycub.log |\
+disp()
+{
 	sed 's/\(^[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*  \?\)\(<[^>]*>\)\(.*\)/\1[32m\2[0m\3/' |\
 	sed 's/\(^[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*  \?\)\(\*\(\*\*\)\?.*$\)/\1[33m\2[0m/' |\
 	less -cNRS
+}
+
+if [ "$1" == "-a" ]; then
+	cat old_logs/* \#bronycub.log | disp
+else
+	cat old_logs/\#bronycub.$(date +%Y%m)* \#bronycub.log | disp
+fi
+
